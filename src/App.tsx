30c88d37b6dcb4a138d4ebc5ec434f58ed396ff6
habibFrh <habibellah.ferhi@univ-constantone2.dz>
2025-09-16 13:54:04 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind 3!</h1>
+    </div>
+  )
+}
+
+export default App
